3c66b91af44fcb9d9c82223b32c3dc052547cfe7
Earth <tunsirivut@gmail.com>
2026-03-13 11:00:36 +0700

    Update grade criteria for production

--- src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
+++ src/main/java/camt/se234/deployment/service/GradeServiceImpl.java
@@ -1,23 +1,20 @@
 package camt.se234.deployment.service;
-
 import camt.se234.deployment.entity.Grade;
 import org.springframework.stereotype.Service;
-
 @Service
 public class GradeServiceImpl implements GradeService {
     @Override
     public String getGrade(double score) {
-        if (score > 79.5) {
+        if (score > 89.5) {
             return "A";
         }
-        else if (score > 74.5){
+        else if (score > 79.5){
             return "B";
-        }else if (score > 59.5){
+        }else if (score > 69.5){
             return "C";
-        }else if (score > 32.5) {
+        }else if (score > 49.5) {
             return "D";
         } else
             return "F";
-
     }
 }
